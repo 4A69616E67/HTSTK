ec0920d98b86aed773d28e26c1a6eb17fad1b2a6
HaoJiang <john-jh@foxmail.com>
2021-06-15 21:03:30 +0800

    Initial commit

---
+++ src/com/github/snowflake/Launcher/Content/Content.java
@@ -0,0 +1,11 @@
+package com.github.snowflake.Launcher.Content;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.FlowPane;
+
+public class Content {
+    public FlowPane ChIP_Seq_FlowPane;
+    public TextArea ChIP_Seq_TextArea;
+
+
+}
